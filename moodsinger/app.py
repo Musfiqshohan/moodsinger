d59d0e115b1c163dc99d567ef096e8558589c9e3
Musfiq Shohan <musfuq14shohan@gmail.comm>
2021-03-17 21:52:53 +0600

    Moodsinger demo platform

--- moodsinger/app.py
+++ moodsinger/app.py
@@ -8,5 +8,16 @@ def hello_world():
     return render_template("index.html")
 
 
+@app.route('/library')
+def load_library():
+    return render_template("library.html")
+
+
+
+@app.route('/upcoming_events')
+def upcoming_events():
+    return render_template("event.html")
+
+
 if __name__ == '__main__':
     app.run()
