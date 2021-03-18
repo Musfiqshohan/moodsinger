8995ab808d76e9ca1571580ce7affbe5640609b2
Musfiq Shohan <musfuq14shohan@gmail.comm>
2021-03-18 06:00:24 +0600

    Moodsinger almost finished

--- moodsinger/app.py
+++ moodsinger/app.py
@@ -8,14 +8,47 @@ def hello_world():
     return render_template("index.html")
 
 
-@app.route('/library')
-def load_library():
-    return render_template("library.html")
 
 
 
 @app.route('/upcoming_events')
 def upcoming_events():
+    return render_template("event.html")
+
+@app.route('/new_release')
+def new_release():
+    return render_template("new_release.html")
+
+
+@app.route('/geo_listen')
+def geo_listen():
+    return render_template("geo_listen.html")
+
+
+@app.route('/subscribe')
+def subscribe():
+    return render_template("subscribe.html")
+
+
+@app.route('/categories')
+def categories():
+    return render_template("categories.html")
+
+
+@app.route('/library')
+def load_library():
+    return render_template("library.html")
+
+@app.route('/notifications')
+def notifications():
+    return render_template("event.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("event.html")
+
+@app.route('/log_out')
+def log_out():
     return render_template("event.html")
 
 
